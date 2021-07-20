5c3a72ce5956956de2369e9f8084370d0365a2bc
bdrsmsdn <badrasam7@gmail.com>
2021-07-21 01:50:51 +0700

    pelayan

---
+++ pages/pelayan/index.php
@@ -0,0 +1,12 @@
+<?php require_once('./header.php') ?>
+        <!-- Main Content -->
+        <div class="main-content">
+          <section class="section">
+            <div class="section-header">
+              <h1>Pesanan</h1>
+            </div>
+          </section>
+        </div>
+        <?php require_once('./footer.php') ?>
+</body>
+</html>
